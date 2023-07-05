e77e65098febc18c134f4191be3f072f88752161
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2023-07-05 13:57:22 -0500

    Ajustements

--- web/sidebars.js
+++ web/sidebars.js
@@ -3,38 +3,165 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   docs: [
-    { type: "doc", label: "Rencontre 1.1", id: "cours/accueil" },
-    { type: "doc", label: "Rencontre 1.2", id: "cours/accueil" },
-    { type: "doc", label: "Rencontre 2.1", id: "cours/imperatif" },
-    { type: "doc", label: "Rencontre 2.2", id: "cours/imperatif" },
-    { type: "doc", label: "Rencontre 3.1", id: "cours/imperatif" },
-    { type: "doc", label: "Rencontre 3.2", id: "cours/integration-1" },
-    { type: "doc", label: "Rencontre 4.1", id: "cours/std-lib" },
-    { type: "doc", label: "Rencontre 4.2", id: "cours/std-lib" },
-    { type: "doc", label: "Rencontre 5.1", id: "cours/librairies" },
-    { type: "doc", label: "Rencontre 5.2", id: "cours/exceptions" },
-    { type: "doc", label: "Rencontre 6.1", id: "cours/formatif-intra" },
-    { type: "doc", label: "Rencontre 6.2", id: "cours/examen" },
-    { type: "doc", label: "Rencontre 7.1", id: "cours/intro-android" },
-    { type: "doc", label: "Rencontre 7.2", id: "cours/activites" },
-    { type: "doc", label: "Rencontre 8.1", id: "cours/menu" },
-    { type: "doc", label: "Rencontre 8.2", id: "cours/recycler" },
-    { type: "doc", label: "Rencontre 9.1", id: "cours/recycler" },
-    { type: "doc", label: "Rencontre 9.2", id: "cours/recycler" },
-    { type: "doc", label: "Rencontre 10.1", id: "cours/integration-2" },
-    { type: "doc", label: "Rencontre 10.2", id: "cours/room" },
-    { type: "doc", label: "Rencontre 11.1", id: "cours/room" },
-    { type: "doc", label: "Rencontre 11.2", id: "cours/room" },
-    { type: "doc", label: "Rencontre 12.1", id: "cours/service" },
-    { type: "doc", label: "Rencontre 12.2", id: "cours/tests" },
-    { type: "doc", label: "Rencontre 13.1", id: "cours/tests" },
-    { type: "doc", label: "Rencontre 13.2", id: "cours/tests" },
-    { type: "doc", label: "Rencontre 14.1", id: "cours/librairies-android" },
-    { type: "doc", label: "Rencontre 14.2", id: "cours/integration-3" },
-    { type: "doc", label: "Rencontre 15.1", id: "cours/formatif-final" },
-    { type: "doc", label: "Rencontre 15.2", id: "cours/examen" },
+    {
+      type: "doc",
+      label: "1.1 - Accueil",
+      id: "cours/accueil",
+    },
+    {
+      type: "doc",
+      label: "1.2 - Accueil",
+      id: "cours/accueil",
+    },
+    {
+      type: "doc",
+      label: "2.1 - Impératif",
+      id: "cours/imperatif",
+    },
+    {
+      type: "doc",
+      label: "2.2 - Impératif",
+      id: "cours/imperatif",
+    },
+    {
+      type: "doc",
+      label: "3.1 - Impératif",
+      id: "cours/imperatif",
+    },
+    {
+      type: "doc",
+      label: "3.2 - Intégration",
+      id: "cours/integration-1",
+    },
+    {
+      type: "doc",
+      label: "4.1 - Librairie standard",
+      id: "cours/std-lib",
+    },
+    {
+      type: "doc",
+      label: "4.2 - Librairie standard",
+      id: "cours/std-lib",
+    },
+    {
+      type: "doc",
+      label: "5.1 - Librairies",
+      id: "cours/librairies",
+    },
+    {
+      type: "doc",
+      label: "5.2 - Exceptions",
+      id: "cours/exceptions",
+    },
+    {
+      type: "doc",
+      label: "6.1 - Formatif",
+      id: "cours/formatif-intra",
+    },
+    {
+      type: "doc",
+      label: "6.2 - Examen Intra",
+      id: "cours/examen",
+      className: "examen",
+    },
+    {
+      type: "doc",
+      label: "7.1 - Introduction Android",
+      id: "cours/intro-android",
+    },
+
+    {
+      type: "doc",
+      label: "7.2 - Activités",
+      id: "cours/activites",
+    },
+    {
+      type: "doc",
+      label: "8.1 - Menu",
+      id: "cours/menu",
+    },
+    {
+      type: "doc",
+      label: "8.2 - Recycler",
+      id: "cours/recycler",
+    },
+    {
+      type: "doc",
+      label: "9.1 - Recycler",
+      id: "cours/recycler",
+    },
+    {
+      type: "doc",
+      label: "9.2 - Recycler",
+      id: "cours/recycler",
+    },
+    {
+      type: "doc",
+      label: "10.1 - Intégration",
+      id: "cours/integration-2",
+    },
+    {
+      type: "doc",
+      label: "10.2 - Room",
+      id: "cours/room",
+    },
+    {
+      type: "doc",
+      label: "11.1 - Room",
+      id: "cours/room",
+    },
+    {
+      type: "doc",
+      label: "11.2 - Room",
+      id: "cours/room",
+    },
+    {
+      type: "doc",
+      label: "12.1 - Service",
+      id: "cours/service",
+    },
+    {
+      type: "doc",
+      label: "12.2 - Tests",
+      id: "cours/tests",
+    },
+    {
+      type: "doc",
+      label: "13.1 - Tests",
+      id: "cours/tests",
+    },
+    {
+      type: "doc",
+      label: "13.2 - Tests",
+      id: "cours/tests",
+    },
+    {
+      type: "doc",
+      label: "14.1 - Librairies Android",
+      id: "cours/librairies-android",
+    },
+    {
+      type: "doc",
+      label: "14.2 - Intégration",
+      id: "cours/integration-3",
+    },
+    {
+      type: "doc",
+      label: "15.1 - Formatif",
+      id: "cours/formatif-final",
+    },
+    {
+      type: "doc",
+      label: "15.2 - Examen Final",
+      id: "cours/examen",
+      className: "examen",
+    },
   ],
-  tp: [{ type: "autogenerated", dirName: "02-tp" }],
+  tp: [
+    { type: "doc", label: "TP1 - ExploraWeb2000", id: "tp/tp1" },
+    { type: "doc", label: "TP2 - Interface graphique Android", id: "tp/tp2" },
+    { type: "doc", label: "TP3 - Appli Android", id: "tp/tp3" },
+  ],
 };
 
 module.exports = sidebars;
