4c966c1dc6f2a0298d12e8551c9349b681e03780
vcarrier <vincent.carrier@cegepmontpetit.ca>
2023-08-23 17:53:19 -0400

    bugfix

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 const config = {
     nom: "420-3A5-EM - Automatisation de tâches",
     description: "Site de référence du cours d'automatisation de tâches'",
-    nomUrl: "3a5-automatisation"
+    nomUrl: "3A5-Automatisation"
 }
 
 module.exports = config;
