59b3e0a4c6178d6042157ff1fd50c87ba050bfe1
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2023-11-16 14:44:38 -0500

    MaJ projets

---
+++ code/TestsExercice2/app/src/main/java/org/depinfo/testsexercice2/MainActivity.java
@@ -0,0 +1,20 @@
+package org.depinfo.testsexercice2;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
