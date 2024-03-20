1546c508511f89bb67bbb8c1264de587b534c1c0
Joris Deguet <joris.deguet@gmail.com>
2024-03-19 23:42:43 -0400

    Projet de Bot pour séance de refactoring

---
+++ code/DasBrokenBot/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.seleniumhq.selenium:selenium-java:4.18.1")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(14)
+}
