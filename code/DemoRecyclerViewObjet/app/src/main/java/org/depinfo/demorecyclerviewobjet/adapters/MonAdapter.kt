b8885e153aa16b40e23cbab0040b510c7dc62dce
PierreOlivierBrillant <pierreolivierbrillant@gmail.com>
2024-05-03 17:08:43 -0400

    tmp

--- code/DemoRecyclerViewObjet/app/src/main/java/org/depinfo/demorecyclerviewobjet/adapters/MonAdapter.kt
+++ code/DemoRecyclerViewObjet/app/src/main/java/org/depinfo/demorecyclerviewobjet/adapters/MonAdapter.kt
@@ -1,4 +1,14 @@
 package org.depinfo.demorecyclerviewobjet.adapters
 
-class MonAdapter {
+import androidx.recyclerview.widget.RecyclerView
+import org.depinfo.demorecyclerviewobjet.databinding.PersonneItemBinding
+
+class PersonneAdapter {
+
+    // binding nous permet d'accéder à tout le champs de notre layout mon_item.xml
+    inner class PersonneItemViewHolder(private val binding: PersonneItemBinding) : RecyclerView.ViewHolder(binding.root) {
+//        fun bind(item: String) {
+//            binding..text = item // On affiche l'élément dans le TextView
+//        }
+    }
 }
---
+++ code/DemoRecyclerViewObjet/app/src/main/java/org/depinfo/demorecyclerviewobjet/model/Personne.kt
@@ -0,0 +1,3 @@
+package org.depinfo.demorecyclerviewobjet.model
+
+data class Personne(val nom: String, val prenom: String, val age: Int, val sexe: String)
